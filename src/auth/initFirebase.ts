2fbe767bca15840bfedb8ee58ea4c1b6cb32c803
Leonardo oteca <lfotecam@gmail.com>
2021-01-23 12:03:43 -0500

    create the login functionality with firebase

--- src/auth/initFirebase.ts
+++ src/auth/initFirebase.ts
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apliKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
 }
 
 export default function initFirebase() {
-  if (!firebase.app.length) {
+  if (!firebase.apps.length) {
     firebase.initializeApp(config)
   }
 }
