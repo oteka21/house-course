35dc91590c5b5a836a35d7070d46c198e6c1c0be
Leonardo oteca <lfotecam@gmail.com>
2021-03-26 18:53:44 -0500

    add house form complete

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect, ChangeEvent } from "react";
+import { useState, useEffect, ChangeEvent, Fragment } from "react";
 import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
-// import Link from "next/link";
+import Link from "next/link";
 // import { Image } from "cloudinary-react";
 import { SearchBox } from "./searchBox";
+import { doTypesOverlap } from "graphql";
 // import {
 //   CreateHouseMutation,
 //   CreateHouseMutationVariables,
@@ -25,8 +26,12 @@ interface IFormData {
 
 interface IProps {}
 
+// TODO:
+// The form doesn't work in onSubmit
+
 export default function HouseForm({}: IProps) {
   const [submitting, setSubmitting] = useState<boolean>(false);
+  const [previewImage, setPreviewImage] = useState<string>("");
   const { register, handleSubmit, setValue, errors, watch } = useForm<
     IFormData
   >({
@@ -39,12 +44,15 @@ export default function HouseForm({}: IProps) {
     register({ name: "longitude" }, { required: true, min: -180, max: -180 });
   }, [register]);
 
-  const handleCreate = async (data: IFormData) => {};
+  const handleCreate = async (data: IFormData) => {
+    console.log(data);
+  };
 
   const onSubmit = (data: IFormData) => {
     setSubmitting(true);
     handleCreate(data);
   };
+
   return (
     <form className="mx-auto max-w-xl py-4" onSubmit={handleSubmit(onSubmit)}>
       <h1>Add a new Hose</h1>
@@ -61,8 +69,79 @@ export default function HouseForm({}: IProps) {
           }}
         />
         {errors.address && <p>{errors.address.message}</p>}
-        <h2>{address}</h2>
       </div>
+      {address && (
+        <Fragment>
+          <div className="mt-4">
+            <label
+              htmlFor="image"
+              className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer"
+            >
+              Click to add image (16:9)
+            </label>
+            <input
+              id="image"
+              name="image"
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              ref={register({
+                validate: (fileList: FileList) => {
+                  if (fileList.length === 1) return true;
+                  return "Please upload one file!";
+                },
+              })}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                if (event?.target?.files?.[0]) {
+                  const file = event.target.files[0];
+                  const reader = new FileReader();
+                  reader.onloadend = () => {
+                    setPreviewImage(reader.result as string);
+                  };
+                  reader.readAsDataURL(file);
+                }
+              }}
+            />
+            {previewImage && (
+              <img
+                src={previewImage}
+                className="mt-4 object-cover"
+                style={{ width: "576px", height: `${(9 / 16) * 576}px` }}
+              />
+            )}
+            {errors.image && <p>{errors.image.message}</p>}
+          </div>
+          <div className="mt-4">
+            <label htmlFor="bedrooms" className="block">
+              Beds
+            </label>
+            <input
+              id="bedrooms"
+              type="number"
+              name="bedrooms"
+              className="p-2"
+              ref={register({
+                required: "Please enter the number of bedrooms",
+                max: { value: 10, message: "Wooahh, too big of a house" },
+                min: { value: 1, message: "mist have at least 1 bedroom" },
+              })}
+            />
+            {errors.bedrooms && <p>{errors.bedrooms.message}</p>}
+          </div>
+          <div className="mt-4">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+              type="submit"
+              disabled={submitting}
+            >
+              Save
+            </button>
+            <Link href="/">
+              <a className="ml-4">Cancel</a>
+            </Link>
+          </div>
+        </Fragment>
+      )}
     </form>
   );
 }
