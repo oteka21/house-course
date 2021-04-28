870cf093b8e01e9a10c67d6d531a8facf9749295
Leonardo oteca <lfotecam@gmail.com>
2021-04-28 09:34:10 -0500

    display houses

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -4,14 +4,16 @@ import { Image } from "cloudinary-react";
 import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useLocalState } from "src/utils/useLocalState";
-// import { HousesQuery_houses } from "src/generated/HousesQuery";
+import { HousesQuery_houses } from "src/generated/HousesQuery";
 // import { SearchBox } from "./searchBox";
 
 interface Iprops {
   setDataBounds: (bounds: string) => void;
+  houses: HousesQuery_houses[];
 }
 
-export default function Map({ setDataBounds }: Iprops) {
+export default function Map({ setDataBounds, houses }: Iprops) {
+  const [selected, setSelected] = useState<HousesQuery_houses | null>(null);
   const mapRef = useRef<ReactMapGL | null>(null);
   const [viewport, setViewport] = useLocalState<ViewState>("viewport", {
     latitude: 6.217,
@@ -43,7 +45,51 @@ export default function Map({ setDataBounds }: Iprops) {
             setDataBounds(JSON.stringify(bounds.toArray()));
           }
         }}
-      ></ReactMapGL>
+      >
+        {houses.map((house) => (
+          <Marker
+            key={house.id}
+            latitude={house.latitude}
+            longitude={house.longitude}
+            offsetLeft={-15}
+            offsetTop={-15}
+          >
+            <button
+              style={{ width: "30px", height: "30px", fontSize: "30px" }}
+              onClick={() => setSelected(house)}
+            >
+              <img src="/home-solid.svg" alt="house" className="w-8" />
+            </button>
+          </Marker>
+        ))}
+        {selected && (
+          <Popup
+            latitude={selected.latitude}
+            longitude={selected.longitude}
+            onClose={() => setSelected(null)}
+            closeOnClick={false}
+          >
+            <div className="text-center">
+              <h3 className="px-4">{selected.address.substr(0, 30)}</h3>
+              <Image
+                className="mx-auto my-4"
+                cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}
+                publicId={selected.publicId}
+                secure
+                dpr="auto"
+                quality="auto"
+                width={200}
+                height={Math.floor((9 / 16) * 200)}
+                crop="fill"
+                gravity="auto"
+              />
+              <Link href={`/houses/${selected.id}`}>
+                <a>View house</a>
+              </Link>
+            </div>
+          </Popup>
+        )}
+      </ReactMapGL>
     </div>
   );
 }
