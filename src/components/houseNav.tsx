40f3aaaf8b29503cacbcc802b810693b0e412e4b
Leonardo oteca <lfotecam@gmail.com>
2021-05-01 12:10:58 -0500

    create the edit page

--- src/components/houseNav.tsx
+++ src/components/houseNav.tsx
@@ -1,7 +1,30 @@
-// import { useMutation, gql } from "@apollo/client";
-// import { useRouter } from "next/router";
-// import Link from "next/link";
-// import { useAuth } from "src/auth/useAuth";
+import { useMutation, gql } from "@apollo/client";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useAuth } from "src/auth/useAuth";
 // import { DeleteHouse, DeleteHouseVariables } from "src/generated/DeleteHouse";
 
-export {};
+interface Iprops {
+  house: {
+    id: string;
+    userId: string;
+  };
+}
+
+export default function HouseNav({ house }: Iprops) {
+  const { user } = useAuth();
+  const canManage = !!user && user.uid === house.userId;
+  return (
+    <>
+      <Link href="/">
+        <a>map</a>
+      </Link>
+      {canManage && (
+        <>
+          {" | "}
+          <Link href={`/houses/${house.id}/edit`}>Edit</Link>
+        </>
+      )}
+    </>
+  );
+}
