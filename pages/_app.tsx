d7376792fb5b2e4fbd2d62e9b172255489d1f9f7
Leonardo oteca <lfotecam@gmail.com>
2021-05-02 20:23:23 -0500

    added router chage progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
+import { useEffect } from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "src/apollo";
 import { AuthProvider, useAuth } from "src/auth/useAuth";
+import { useRouter, Router } from "next/router";
+import NProgress from "nprogress";
 import "../styles/index.css";
 
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
 export default function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo();
+
   return (
     <AuthProvider>
       <ApolloProvider client={client}>
